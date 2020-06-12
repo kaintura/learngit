73c220fa6f7dd351f1abf3935a534ed9266a3b6d
ashish kaintura <ashish.kaintura@gmail.com>
2020-06-12 13:00:43 +0530

    add calc file

---
+++ calc.php
@@ -0,0 +1,17 @@
+<?php
+
+function add($a,$b){
+	return $a + $b;
+}
+
+function sub($a,$b){
+	return $a - $b;
+}
+
+function multi($a,$b){
+	return $a * $b;
+}
+
+function divide($a,$b){
+	return $a / $b;
+}
